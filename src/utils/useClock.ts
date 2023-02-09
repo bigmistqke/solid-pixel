ba0a4dfee74593c61702f0a41116e2aecde216fd
bigmistqke <vandijckv@gmail.com>
2023-02-09 19:03:19 +0100

    useClock: initiate with fps

--- src/utils/useClock.ts
+++ src/utils/useClock.ts
@@ -1,17 +1,23 @@
 import { createSignal, onCleanup } from 'solid-js'
 
-export default () => {
+export default (fps = 33.3333) => {
   const [clock, setClock] = createSignal(0)
   let last = performance.now()
   const tick = () => {
     setClock(c => c + 1)
     last = performance.now()
   }
+  let interval: NodeJS.Timer
   return {
     clock,
-    start: (fps: number) => {
-      const interval = setInterval(tick, fps)
+    start: () => {
+      if (interval) clearInterval(interval)
+      interval = setInterval(tick, fps)
       onCleanup(() => clearInterval(interval))
     },
+    stop: () => {
+      if (interval) clearInterval(interval)
+    },
+    tick,
   }
 }
